e6db70bd8293527e972359f9babc94b3b1f98aab
Fabian Portilla <fas_456@hotmail.com>
2022-09-13 08:25:01 -0500

    Add a results component

---
+++ src/app/gifs/results/results.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styles: [
+  ]
+})
+export class ResultsComponent {
+
+  constructor() { }
+
+}
