4b5eef6b0abec51eca337f8df1618d8bff67e337
Fabian Portilla <fas_456@hotmail.com>
2022-09-13 09:17:49 -0500

    Add http service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, GifsResponse } from 'src/app/gifs/interfaces/gifsResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private _history: string[] = []
+  private apiUrl: string = 'https://api.giphy.com/v1/gifs/search?api_key='
+  private apiKey: string = '1vR17fUGqB05RUdCT1fW2W0JdrDbFGtY';
+  private _history: string[] = [];
+  public results: Gif[] = []
 
   get history() {
     return [...this._history]
   }
+
+  constructor(private http: HttpClient) { }
 
   searchGifs(query: string = '') {
 
@@ -19,6 +26,13 @@ export class GifsService {
       this._history.unshift(query)
       this._history = this._history.splice(0, 9)
     }
+
+    this.http.get<GifsResponse>(`${this.apiUrl}${this.apiKey}&q=${query}&limit=10`)
+      .subscribe(res => {
+        this.results = res.data
+      })
+
+
   }
 
 }
